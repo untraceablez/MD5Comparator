c0a0e68609c0cdbc2a225d872bd6186d3f26e6a8
Taylor Cohron <untraceablez@users.noreply.github.com>
2020-06-04 11:57:30 -0500

    Create MD5Comparator.ps1

---
+++ MD5Comparator.ps1
@@ -0,0 +1,32 @@
+﻿#MD5 Hash Comparator
+
+
+#Select original file
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    
+}
+$null = $FileBrowser.ShowDialog()
++
+#Store MD5 hash in variable
+
+$og_md5 = Get-FileHash OpenFileDialog
+
+#Select destination file
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    
+}
+$null = $FileBrowser.ShowDialog()
+
+$new_md5 = Get-FileHash OpenFileDialog
+
+#Compare Files
+
+if(Compare-Object -ReferenceObject $(Get-Content $og_md5) -DifferenceObject $(Get-Content $new_md5))
+
+ {[System.Windows.MessageBox]::Show('MD5 Hashes did not match, error in transfer.')}
+
+Else {[System.Windows.MessageBox]::Show('File safely transferred, MD5 hash is identical.')}
